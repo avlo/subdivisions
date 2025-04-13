c03a54dc2f58c458aa5fef8cffda75420456099e
nick avlo <avlo@yahoo.com>
2025-04-12 19:27:50 -0700

    added a NostrRelayService

---
+++ src/main/java/com/prosilion/subdivisions/service/NostrRelayService.java
@@ -0,0 +1,123 @@
+package com.prosilion.subdivisions.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.collect.Streams;
+import com.prosilion.subdivisions.event.EventPublisher;
+import com.prosilion.subdivisions.request.RelaySubscriptionsManager;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import nostr.api.factory.impl.NIP01Impl.EventMessageFactory;
+import nostr.base.Command;
+import nostr.event.impl.GenericEvent;
+import nostr.event.json.codec.BaseMessageDecoder;
+import nostr.event.message.EventMessage;
+import nostr.event.message.OkMessage;
+import nostr.event.message.ReqMessage;
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class NostrRelayService {
+  private final EventPublisher eventPublisher;
+  private final RelaySubscriptionsManager relaySubscriptionsManager;
+
+  public NostrRelayService(@NonNull String relayUri) throws ExecutionException, InterruptedException {
+    log.debug("relayUri: \n{}", relayUri);
+    this.eventPublisher = new EventPublisher(relayUri);
+    this.relaySubscriptionsManager = new RelaySubscriptionsManager(relayUri);
+  }
+
+  public NostrRelayService(@NonNull String relayUri, SslBundles sslBundles
+  ) throws ExecutionException, InterruptedException {
+    log.debug("relayUri: \n{}", relayUri);
+    log.debug("sslBundles: \n{}", sslBundles);
+    final SslBundle server = sslBundles.getBundle("server");
+    log.debug("sslBundles name: \n{}", server);
+    log.debug("sslBundles key: \n{}", server.getKey());
+    log.debug("sslBundles protocol: \n{}", server.getProtocol());
+    this.eventPublisher = new EventPublisher(relayUri, sslBundles);
+    this.relaySubscriptionsManager = new RelaySubscriptionsManager(relayUri, sslBundles);
+  }
+
+  public void createEvent(@NonNull String eventJson) throws IOException {
+    assert (
+        getOkMessage(
+            sendEvent(eventJson)
+        ).getFlag());
+  }
+
+  public void createEvent(@NonNull GenericEvent event) throws IOException {
+    assert (
+        getOkMessage(
+            sendEvent(
+                new EventMessageFactory(event).create())
+        ).getFlag());
+  }
+
+  private static OkMessage getOkMessage(List<String> received) {
+    return Streams.findLast(received.stream())
+        .map(baseMessage -> {
+          try {
+            return new BaseMessageDecoder<OkMessage>().decode(baseMessage);
+          } catch (JsonProcessingException e) {
+            return null;
+          }
+        })
+        .orElseThrow();
+  }
+
+  private List<String> sendEvent(String eventJson) throws IOException {
+    eventPublisher.createEvent(eventJson);
+    log.debug("socket send event JSON content\n  {}", eventJson);
+    return getEvents();
+  }
+
+  private List<String> sendEvent(EventMessage eventMessage) throws IOException {
+    eventPublisher.sendEvent(eventMessage);
+    log.debug("socket send EventMessage content\n  {}", eventMessage.getEvent());
+    return getEvents();
+  }
+
+  public List<String> getEvents() {
+    List<String> events = eventPublisher.getEvents();
+    log.debug("received relay response:");
+    log.debug("\n" + events.stream().map(event -> String.format("  %s\n", event)).collect(Collectors.joining()));
+    return events;
+  }
+
+  public List<String> sendRequestReturnEvents(@NonNull ReqMessage reqMessage) throws JsonProcessingException {
+    return relaySubscriptionsManager.sendRequestReturnEvents(reqMessage);
+  }
+
+  public List<String> sendRequestReturnEvents(@NonNull String reqJson, @NonNull String clientUuid) {
+    List<String> returnedEvents = relaySubscriptionsManager.sendRequestReturnEvents(clientUuid, reqJson);
+    log.debug("-------------");
+    log.debug("socket [{}] getEvents():", clientUuid);
+    returnedEvents.forEach(event -> log.debug("  {}\n", event));
+    log.debug("222222222222\n");
+    return returnedEvents;
+  }
+
+  public Map<Command, Optional<String>> sendRequest(@NonNull String reqJson, @NonNull String clientUuid) {
+    Map<Command, List<String>> resultsMap = relaySubscriptionsManager.sendRequestReturnCommandResultsMap(clientUuid, reqJson);
+    List<String> returnedEvents = resultsMap.get(Command.EVENT);
+    log.debug("socket [{}] getEvents():", clientUuid);
+    returnedEvents.forEach(event -> log.debug("  {}\n", event));
+    log.debug("222222222222\n");
+    String joined = String.join(",", returnedEvents);
+    log.debug("-------------");
+    Map<Command, Optional<String>> returnMap = new HashMap<>();
+    returnMap.put(Command.EOSE, Optional.of(resultsMap.get(Command.EOSE).getFirst()));
+    Optional<String> value = Optional.of(joined).orElseThrow().isEmpty() ? Optional.empty() : Optional.of(joined);
+    returnMap.put(Command.EVENT, value);
+    return returnMap;
+  }
+}
---
+++ src/test/java/com/prosilion/subdivisions/service/NostrRelayServiceTest.java
@@ -0,0 +1,76 @@
+package com.prosilion.subdivisions.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.prosilion.subdivisions.util.Factory;
+import lombok.extern.slf4j.Slf4j;
+import nostr.base.PublicKey;
+import nostr.event.filter.AuthorFilter;
+import nostr.event.filter.EventFilter;
+import nostr.event.filter.Filters;
+import nostr.event.impl.GenericEvent;
+import nostr.event.message.ReqMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:application-test.properties")
+@ActiveProfiles("test")
+public class NostrRelayServiceTest {
+  private final NostrRelayService nostrRelayService;
+
+  private final static String authorPubKey = Factory.generateRandomHex64String();
+  private final static String eventId = Factory.generateRandomHex64String();
+  private final static String globalSubscriberId = Factory.generateRandomHex64String(); // global subscriber UUID
+  private final String content;
+
+  @Autowired
+  public NostrRelayServiceTest(@Value("${superconductor.relay.uri}") String relayUri) throws IOException, ExecutionException, InterruptedException {
+    this.nostrRelayService = new NostrRelayService(relayUri);
+    content = Factory.lorumIpsum(getClass());
+
+    String globalEventJson = "[\"EVENT\",{\"id\":\"" + eventId + "\",\"kind\":1,\"content\":\"" + content + "\",\"pubkey\":\"" + authorPubKey + "\",\"created_at\":1717357053050,\"tags\":[],\"sig\":\"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546\"}]";
+    log.debug("setup() send event:\n  {}", globalEventJson);
+
+    this.nostrRelayService.createEvent(globalEventJson);
+    assertFalse(this.nostrRelayService.getEvents().isEmpty());
+  }
+
+  @Test
+  void testReqFilteredByEventAndAuthorViaReqMessage() throws JsonProcessingException {
+    final String subscriberId = Factory.generateRandomHex64String();
+
+    EventFilter<GenericEvent> eventFilter = new EventFilter<>(new GenericEvent(eventId));
+    AuthorFilter<PublicKey> authorFilter = new AuthorFilter<>(new PublicKey(authorPubKey));
+
+    ReqMessage reqMessage = new ReqMessage(subscriberId, new Filters(eventFilter, authorFilter));
+    List<String> returnedEvents = nostrRelayService.sendRequestReturnEvents(reqMessage);
+
+    log.debug("okMessage to UniqueSubscriberId:");
+    log.debug("  " + returnedEvents);
+    assertTrue(returnedEvents.stream().anyMatch(event -> event.contains(eventId)));
+    assertTrue(returnedEvents.stream().anyMatch(event -> event.contains(content)));
+    assertTrue(returnedEvents.stream().anyMatch(event -> event.contains(authorPubKey)));
+
+    ReqMessage reqMessage2 = new ReqMessage(globalSubscriberId, new Filters(eventFilter, authorFilter));
+    List<String> returnedEvents2 = nostrRelayService.sendRequestReturnEvents(reqMessage2);
+
+    log.debug("okMessage:");
+    log.debug("  " + returnedEvents2);
+    assertTrue(returnedEvents2.stream().anyMatch(event -> event.contains(eventId)));
+    assertTrue(returnedEvents2.stream().anyMatch(event -> event.contains(content)));
+    assertTrue(returnedEvents2.stream().anyMatch(event -> event.contains(authorPubKey)));
+  }
+}
