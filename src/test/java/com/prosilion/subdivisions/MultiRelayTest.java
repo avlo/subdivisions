0c48c2f67b2deb9145e7173bda221622c51b5e4b
nick avlo <avlo@yahoo.com>
2025-05-03 17:27:35 -0700

    add tests

--- src/test/java/com/prosilion/subdivisions/MultiRelayTest.java
+++ src/test/java/com/prosilion/subdivisions/MultiRelayTest.java
@@ -1,7 +1,7 @@
 package com.prosilion.subdivisions;
 
 import com.prosilion.subdivisions.config.SuperconductorRelayConfig;
-import com.prosilion.subdivisions.event.EventPublisher;
+import com.prosilion.subdivisions.event.StandardEventPublisher;
 import com.prosilion.subdivisions.util.Factory;
 import lombok.extern.slf4j.Slf4j;
 import nostr.base.PublicKey;
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import org.testcontainers.containers.ComposeContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,15 +28,25 @@ public class MultiRelayTest {
   private final Map<String, String> superconductorRelays;
 
   @Autowired
-  public MultiRelayTest(Map<String, String> superconductorRelays) {
+  public MultiRelayTest(
+      ComposeContainer superconductorContainer,
+      Map<String, String> superconductorRelays) {
+    String serviceHost = superconductorContainer.getServiceHost("superconductor-subdivisions", 5555);
+
+    log.debug("11111111111111111");
+    log.debug("11111111111111111");
+    log.debug("serviceHost: {}", serviceHost);
+    log.debug("11111111111111111");
+    log.debug("11111111111111111");
+    
     this.superconductorRelays = superconductorRelays;
     superconductorRelays.forEach((k, v) -> log.info("\n\nrelayName:\n  {}\nrelayUri\n  {}\n", k, v));
   }
 
   @Test
   void multiRelayTest() throws IOException {
-    List<EventPublisher> eventPublishers = Streams.failableStream(superconductorRelays.values()).map(EventPublisher::new).stream().toList();
-    for (EventPublisher eventPublisher : eventPublishers) {
+    List<StandardEventPublisher> standardEventPublishers = Streams.failableStream(superconductorRelays.values()).map(StandardEventPublisher::new).stream().toList();
+    for (StandardEventPublisher standardEventPublisher : standardEventPublishers) {
       String eventId = Factory.generateRandomHex64String();
       Identity newIdentity = Factory.createNewIdentity();
       PublicKey authorPubKey = newIdentity.getPublicKey();
@@ -51,7 +62,7 @@ public class MultiRelayTest {
               ",sig:\"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546\"}]";
       log.info("multiRelayTest() send event:\n  {}", globalEventJson);
 
-      OkMessage okMessage = eventPublisher.sendEvent(globalEventJson);
+      OkMessage okMessage = standardEventPublisher.sendEvent(globalEventJson);
       assertTrue(okMessage.getFlag());
       assertEquals(eventId, okMessage.getEventId());
       assertEquals("success: request processed", okMessage.getMessage());
