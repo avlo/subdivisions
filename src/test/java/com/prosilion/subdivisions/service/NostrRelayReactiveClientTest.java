03117818af3e3529928e8afbf359935275dab40c
nick avlo <avlo@yahoo.com>
2025-05-09 20:54:02 -0700

    embellish SampleSubscriber extends BaseSubscriber

--- src/test/java/com/prosilion/subdivisions/service/NostrRelayReactiveClientTest.java
+++ src/test/java/com/prosilion/subdivisions/service/NostrRelayReactiveClientTest.java
@@ -5,8 +5,12 @@ import com.prosilion.subdivisions.config.SuperconductorRelayConfig;
 import com.prosilion.subdivisions.util.Factory;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.Getter;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import nostr.api.NIP01;
 import nostr.base.PublicKey;
@@ -18,6 +22,7 @@ import nostr.event.message.EventMessage;
 import nostr.event.message.ReqMessage;
 import nostr.id.Identity;
 import org.apache.commons.lang3.StringUtils;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.reactivestreams.Subscription;
@@ -28,7 +33,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
+import reactor.core.publisher.SignalType;
+import reactor.util.context.Context;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -40,14 +46,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ActiveProfiles("test")
 class NostrRelayReactiveClientTest {
   public static final String ANSI_YELLOW = "\033[1;93m";
-  ;
   public static final String ANSI_RESET = "\u001B[0m";
   public static final String ANSI_BLUE = "\033[1;34m";
   public static final String ANSI_RED = "\033[1;36m";
   public static final String ANSI_35 = "\033[1;35m";
   //  public static final String ANSI_RED = "\033[0;36m";
   private final ReactiveNostrRelayClient reactiveNostrRelayClient;
-  private final static String globalSubscriberId = Factory.generateRandomHex64String();
   private final String relayUri;
 
   public NostrRelayReactiveClientTest(@Value("${superconductor.relay.uri}") String relayUri) {
@@ -61,22 +65,28 @@ class NostrRelayReactiveClientTest {
    */
   @Test
   void testEventCreationAndSubscriptionUsingExplicitSubscriber() throws IOException, InterruptedException {
+    log.debug("AAAAAAAAAAAAAAAAAAAAAAA");
+    log.debug("AAAAAAAAAAAAAAAAAAAAAAA");
+
     Identity identity = Factory.createNewIdentity();
     GenericEvent event = new NIP01<>(identity).createTextNoteEvent(Factory.lorumIpsum()).sign().getEvent();
     log.trace("setup() send event:\n  {}", event.toString());
 
     Flux<String> flux = reactiveNostrRelayClient.sendEvent(new EventMessage(event, event.getId()));
 
-    log.debug("AAAAAAAAAAAAAAAAAAAAAAA");
     printConsole(flux.hashCode());
-    log.debug("AAAAAAAAAAAAAAAAAAAAAAA");
 
 //  1) ******* if only below SampleSubscriber is active, OnNext will register 1 EVENT
     SampleSubscriber<String> eventSubscriber = new SampleSubscriber<>();
     flux.subscribe(eventSubscriber);
+//    eventSubscriber.dispose();
+
+    List<String> items = eventSubscriber.getItems();
+    String expected = "[\"OK\",\"" + event.getId() + "\",true,\"success: request processed\"]";
+    assertEquals(expected, items.getFirst());
 
 //  2) ******* if only below collectList() is active, OnNext will register 0 EVENTS
-//     note: including below with above subscriber causes TWO EVENTs to become sent
+//     note: including below with above subscriber causes sending of TWO EVENTs
 //    List<String> list3 = new ArrayList<>();
 //    flux.collectList().subscribe(list3::addAll);
 //  3) ******* if both above SampleSubscriber and collectList are active, OnNext will register 2 EVENTS w/ same ID
@@ -85,6 +95,9 @@ class NostrRelayReactiveClientTest {
 ////    assertEquals(1, list3.stream().filter(s -> s.contains(expected)).count());
 //    assertEquals(1, list3.size());
     TimeUnit.MILLISECONDS.sleep(250);
+
+    log.debug("AAAAAAAAAAAAAAAAAAAAAAA");
+    log.debug("AAAAAAAAAAAAAAAAAAAAAAA");
   }
 
   /*
@@ -92,28 +105,25 @@ class NostrRelayReactiveClientTest {
    *  StepVerifier does not register subscription event
    */
   @Test
-  void testEventCreationAndSubscriptionUsingStepVerifier() throws IOException, InterruptedException {
+  void testEventCreationAndSubscriptionUsingBlockFirst() throws IOException, InterruptedException {
+    log.debug("\nBBBBBBBBBBBBBBBBBBBBBBB");
+    log.debug("BBBBBBBBBBBBBBBBBBBBBBB");
     Identity identity = Factory.createNewIdentity();
     GenericEvent event = new NIP01<>(identity).createTextNoteEvent(Factory.lorumIpsum()).sign().getEvent();
     log.trace("setup() send event:\n  {}", event.toString());
 
     Flux<String> flux = reactiveNostrRelayClient.sendEvent(new EventMessage(event, event.getId()));
-
-    log.debug("BBBBBBBBBBBBBBBBBBBBBBB");
     printConsole(flux.hashCode());
-    log.debug("BBBBBBBBBBBBBBBBBBBBBBB");
-    SampleSubscriber<String> eventSubscriber = new SampleSubscriber<>();
-    flux.subscribe(eventSubscriber);
+
+//    example of blockFirst directly on a flux
+    String returnedEventReq = flux.blockFirst();
 
     String expected = "[\"OK\",\"" + event.getId() + "\",true,\"success: request processed\"]";
-    StepVerifier
-        .create(flux)
-        .expectSubscription()
-        .expectNext(expected)
-//        .expectNext("done") //        .expectErrorMessage("boom")
-//        .verifyComplete(); //        .expectComplete() //        .verify();
-    ;
+    assertEquals(expected, returnedEventReq);
+
     TimeUnit.MILLISECONDS.sleep(250);
+    log.debug("BBBBBBBBBBBBBBBBBBBBBBB");
+    log.debug("BBBBBBBBBBBBBBBBBBBBBBB\n");
   }
 
   /*
@@ -253,8 +263,8 @@ class NostrRelayReactiveClientTest {
     assertEquals(returnedReqGenericEvent.getId(), event.getId());
     assertEquals(returnedReqGenericEvent.getContent(), event.getContent());
     assertEquals(returnedReqGenericEvent.getPubKey().toHexString(), event.getPubKey().toHexString());
-    log.debug("DDDDDDDDDDDDDDDDDDDDDDD");
-    log.debug("DDDDDDDDDDDDDDDDDDDDDDD\n");
+    log.debug("EEEEEEEEEEEEEEEEEEEEEEEE");
+    log.debug("EEEEEEEEEEEEEEEEEEEEEEEE\n");
   }
 
   final void printConsole(int i) {
@@ -267,24 +277,66 @@ class NostrRelayReactiveClientTest {
     log.debug(" " + ANSI_35 + StringUtils.stripEnd(strippedStart, "\"") + ANSI_RESET + " " + value.hashCode());
   }
 
+  @Getter
   private class SampleSubscriber<T> extends BaseSubscriber<T> {
-    public void hookOnSubscribe(Subscription subscription) {
-//      log.debug();
-//      log.debug("0000000000000000000000");
-//      log.debug(" Subscription object hashCode: [ " + ANSI_BLUE + subscription.hashCode() + ANSI_RESET + " ]");
-//      log.debug("0000000000000000000000");
-//      log.debug();
-      request(1);
+    private List<T> items;
+    private AtomicBoolean completed;
+
+    @Override
+    public void hookOnSubscribe(@NonNull Subscription subscription) {
+      this.items = Collections.synchronizedList(new ArrayList<>());
+      this.completed = new AtomicBoolean(false);
+
+      requestUnbounded();
+
+      log.debug("0000000000000000000000");
+      log.debug(" Subscription object hashCode: [ " + ANSI_BLUE + subscription.hashCode() + ANSI_RESET + " ]");
+      log.debug("0000000000000000000000");
     }
 
-    public void hookOnNext(T value) {
-//      log.debug("111111111111111111111");
-//      String subscriberId = value.toString().split(",")[1];
-//      String strippedStart = StringUtils.stripStart(subscriberId, "\"");
-//      log.debug(" On Next: " + ANSI_RED + StringUtils.stripEnd(strippedStart, "\"") + ANSI_RESET + " " + value.hashCode());
-      request(1);
-//      log.debug("111111111111111111111");
-//      log.debug();
+    @Override
+    public void hookOnNext(@NonNull T value) {
+      requestUnbounded();
+
+      log.debug("111111111111111111111");
+      String subscriberId = value.toString().split(",")[1];
+      String strippedStart = StringUtils.stripStart(subscriberId, "\"");
+      log.debug(" On Next: " + ANSI_RED + StringUtils.stripEnd(strippedStart, "\"") + ANSI_RESET + " " + value.hashCode());
+      log.debug("---------------------");
+      completed.setRelease(false);
+      items.add(value);
+      completed.setRelease(true);
+      log.debug("item list:");
+      items.forEach(item -> log.debug("  " + item.toString()));
+      log.debug("111111111111111111111");
     }
+
+    public List<T> getItems() {
+      Awaitility.await()
+          .timeout(1, TimeUnit.MINUTES)
+          .untilTrue(completed);
+//      completed.setRelease(false);
+      List<T> eventList = List.copyOf(items);
+      items.clear();
+      return eventList;
+    }
+
+    //    below included only informatively / as reminder of their existence
+    @Override
+    protected void hookOnCancel() { super.hookOnCancel(); }
+    @Override
+    protected void hookOnComplete() { super.hookOnComplete(); }
+    @Override
+    protected void hookOnError(Throwable throwable) { super.hookOnError(throwable); }
+    @Override
+    protected void hookFinally(SignalType type) { super.hookFinally(type); }
+    @Override
+    public void dispose() { super.dispose(); }
+    @Override
+    public boolean isDisposed() { return super.isDisposed(); }
+    @Override
+    protected Subscription upstream() { return super.upstream(); }
+    @Override
+    public Context currentContext() { return super.currentContext(); }
   }
 }
