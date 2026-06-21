e820f3388a5c36669001a9bb203e74b93478b523
nick avlo <avlo@yahoo.com>
2026-06-20 17:56:09 -0700

    subscription getter

--- src/main/java/com/prosilion/subdivisions/client/RequestSubscriberDelegate.java
+++ src/main/java/com/prosilion/subdivisions/client/RequestSubscriberDelegate.java
@@ -1,19 +1,19 @@
 package com.prosilion.subdivisions.client;
 
-import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 
-@Getter
+import static com.prosilion.subdivisions.client.RequestSubscriber.await;
+
 @Slf4j
 public class RequestSubscriberDelegate<T> extends BaseSubscriber<T> {
   public final static long DEFAULT_REQUEST_COUNT = Long.MAX_VALUE;
 
   private final RequestSubscriberDelegateIF<T> requestSubscriberDelegateIF;
   private final long requestCount;
-  
+
   private Subscription subscription;
 
   public RequestSubscriberDelegate(@NonNull RequestSubscriberDelegateIF<T> requestSubscriberDelegateIF) {
@@ -25,6 +25,12 @@ public class RequestSubscriberDelegate<T> extends BaseSubscriber<T> {
     this.requestCount = requestCount;
   }
 
+  public Subscription getSubscription() {
+    await(requestSubscriberDelegateIF.getTimeout(), () ->
+       subscription != null);
+    return subscription;
+  }
+
   @Override
   public void hookOnSubscribe(@NonNull Subscription subscription) {
     this.subscription = subscription;
@@ -33,7 +39,7 @@ public class RequestSubscriberDelegate<T> extends BaseSubscriber<T> {
 
   @Override
   public void hookOnNext(@NonNull T value) {
-    requestSubscriberDelegateIF.doDelegate(value);
+    requestSubscriberDelegateIF.doDelegate(value, subscription);
     subscription.request(requestCount);
   }
 
--- src/main/java/com/prosilion/subdivisions/client/RequestSubscriberDelegateIF.java
+++ src/main/java/com/prosilion/subdivisions/client/RequestSubscriberDelegateIF.java
@@ -1,8 +1,11 @@
 package com.prosilion.subdivisions.client;
 
+import java.time.Duration;
 import lombok.NonNull;
+import org.reactivestreams.Subscription;
 
 public interface RequestSubscriberDelegateIF<T> {
-  void doDelegate(@NonNull T t);
+  void doDelegate(@NonNull T t, @NonNull Subscription subscription);
   void dispose();
+  Duration getTimeout();
 }
